6c8c5c15478213cb327545c55158eb2d466e94e0
Marosey09 <145623689+Marosey09@users.noreply.github.com>
2024-04-13 09:13:02 -0400

    Update latinTranslate.cpp

--- latinTranslate.cpp
+++ latinTranslate.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main ()
 {
-    cout << "Hello World" << endl;
+    string text;
+    string text2;
+    string text3;
+    string text4;
+    string text5;
+    string text6;
+    string text7;
+
+    cout << "Please enter text for translation" << endl;
+    cin >> text;
+    cin >> text2;
+
+
+    if (text == "meum");
+    {
+        cout << "my";
+    }
+
+    if (text2 == "nomen");
+    {
+        cout << "name";
+    }
+ 
+    if (text3 == "est" );
+    {
+        cout << "is ";
+    }
+
+
+
+    
+
 }
+
